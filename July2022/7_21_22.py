74cee6f1b5d7e30ff8a6ee53f38d48092343d5ed
nwelter1 <nwelter1@gmail.com>
2022-07-21 10:58:45 -0500

    reverse LL

---
+++ July2022/7_21_22.py
@@ -0,0 +1,35 @@
+# Starter code -- LL builder class
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+    def insert(self, val):
+        curr = self
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(val)
+# Building list for example 1
+head1 = ListNode(1)
+head1.insert(2)
+head1.insert(3)
+head1.insert(4)
+head1.insert(5)
+# Building list for example 2
+head2 = ListNode(1)
+head2.insert(2)
+# Your code here
+# O(n) Time | O(1) Space where n == len of LL
+def reverseList(head):
+    if not head:
+        return None
+    prev = None
+    curr = head
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    return prev
+
+print(reverseList(head2).next.val)
+
